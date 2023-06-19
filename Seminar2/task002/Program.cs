baf5345972d9efc5fb0583f744017dcc8e5462a3
oleg.ivakin <oleg_ivakin@mail.ru>
2023-06-19 17:28:36 +0600

    Второй семинар

---
+++ Seminar2/task002/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a ;
+string result;
+
+void res (int  num , int pos){
+result = num.ToString();
+Console.WriteLine( result[pos]);
+}
+
+while (true){
+    
+  Console.WriteLine("введите трехзначное число ");  
+  a = Convert.ToInt32(Console.ReadLine());
+  if (a > 99)  
+  {
+    res(a,2);
+    break;
+  }
+  else 
+  {
+     Console.WriteLine("третья цифра отсутствует! "); 
+  }
+}
+
+
---
+++ Seminar2/task003/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+int a ;
+  
+void validate (int  num )
+    {
+    if (num < 1 || num > 7)
+        {
+        Console.WriteLine("Введенное число не попадает в  диапозон от 1 до 7 "); 
+        }
+    else if (num >=6)
+        {
+        Console.WriteLine("Сегодня Выходной! ");
+        }
+    else
+        {
+        Console.WriteLine("Сегодня рабочий день! ");
+        }    
+        }
+
+    Console.WriteLine("введите число обозначающее день недели от 1 до 7 ");
+
+    a = Convert.ToInt32(Console.ReadLine());
+
+    validate(a);
+
+
+
+
+
+
