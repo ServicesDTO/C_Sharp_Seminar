ffda8601816ec2a9a99f4fcedd38f91f3f33ce44
oleg.ivakin <oleg_ivakin@mail.ru>
2023-06-20 23:50:36 +0600

    Третий семинар

---
+++ Seminar3/task001/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+int a;
+
+
+ String  IsPalindrome(int x ){
+    int revertedNumber = 0;
+    while(x > revertedNumber)
+    {
+        revertedNumber = revertedNumber * 10 + x % 10;
+        x /= 10;
+    }
+    if(x == revertedNumber || x == revertedNumber/10)
+    {
+        return "Является!";
+    }
+    else 
+    {
+        return "Не является!";
+    }
+}
+
+while (true){
+    
+  Console.WriteLine("введите трехзначное число ");  
+  a = Convert.ToInt32(Console.ReadLine());
+  if (a >= 10000 && a <= 99999 ){ 
+    System.Console.WriteLine(IsPalindrome(a));
+  }
+}
---
+++ Seminar3/task002/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+Console.WriteLine("Введите значение координат первой точки");
+int[] a = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+Console.WriteLine("Введите значение координат второй точки");
+int[] b = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+Console.WriteLine("расстояние между ними в 3D пространстве = "
+                + Math.Sqrt(Math.Pow(b[0] - a[0],2) + 
+                            Math.Pow(b[1] - a[1],2) +
+                            Math.Pow(b[2] - a[2],2)
+                           )
+                 );
