84f92a64aa9f42d7dd4e612bf45a6d08bdcc4666
oleg.ivakin <oleg_ivakin@mail.ru>
2023-07-09 23:40:49 +0600

    seminar 6

---
+++ Seminar6/task001/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+
+Console.WriteLine("Введите любые числа через запятую");
+string input = Console.ReadLine();
+
+int[] ConvertToArray(string inputStr){
+
+    int[] intArray =  Array.ConvertAll(input.Split(','), int.Parse);
+    return intArray;
+}
+
+void CountPositivValues(int[] inputArray)
+{
+int result = 0 ;
+foreach (int i in ConvertToArray(input)){
+    if (i>0){
+        result++;
+    }
+}
+    System.Console.WriteLine("Количесво чисел  > 0 равно " + result);
+}
+
+CountPositivValues(ConvertToArray(input));
---
+++ Seminar6/task002/Program.cs
@@ -0,0 +1,31 @@
+﻿ /*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+Console.WriteLine("Введите значения переменных (b1 , k1 , b2 , k2) - через запятую");
+string input = Console.ReadLine();
+
+double[] ConvertToArray(string inputStr){
+
+    double[] intArray =  Array.ConvertAll(input.Split(','), double.Parse);
+    foreach(double i in intArray){
+        System.Console.WriteLine( i);
+    }
+    return intArray;
+}
+
+void FindCoordinates(double[] inputValues){
+
+    double xCoordinate = 0.0;
+    double yCoordinate = 0.0;
+     //k1 * x + b1 =  k2 * x + b2 -> k1x - k2x = b2 - b1 -> x(k1 -k2)= b2 -b1 -> x=(b2-b1)/k1-k2
+     xCoordinate = Math.Round((inputValues[2] - inputValues[0] / inputValues[1] - inputValues[3]),2);
+     yCoordinate = Math.Round((inputValues[1] * xCoordinate + inputValues[0]),2);
+     System.Console.WriteLine(" точка пересечения двух прямых ("+ xCoordinate +","+ yCoordinate+")");
+}
+FindCoordinates(ConvertToArray(input));
+
+
+
