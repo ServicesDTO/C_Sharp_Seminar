c61c0b02da4e5943b29ad334bf730cde8aacdfd4
oleg.ivakin <oleg_ivakin@mail.ru>
2023-07-12 23:06:59 +0600

    Seminar9

---
+++ Seminar9/task001/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+int sum = 0;
+
+int ReadInt(string message){
+    System.Console.Write(message + ": ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+
+void FindSum(int firstValue,int secondValue){
+    
+    if ( firstValue ==  secondValue){
+        System.Console.WriteLine("значение равны");
+        sum = sum + firstValue;
+        return;}
+        sum = sum + firstValue;
+     FindSum(firstValue + 1,secondValue)  ;
+    return;
+    
+}
+
+int M = ReadInt("Введите первое число ");
+int N = ReadInt("Введите первое число ");
+
+FindSum(M,N);
+System.Console.WriteLine("Сумма чисел от "+ M + " до " + N + " = "+ sum);
+
+
---
+++ Seminar9/task002/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int ReadInt(string message){
+    System.Console.Write(message + ": ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Akkerman(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return Akkerman(n - 1, 1);
+    else
+      return Akkerman(n - 1, Akkerman(n, m - 1));
+}
+
+
+int N = ReadInt("Введите первое число ");
+int M = ReadInt("Введите первое число ");
+
+
+System.Console.WriteLine(Akkerman(N,M));
+
